74b8eb8f79ecdb1450aeeea980bfb7d7c0db4023
ESMENIELD <esmeralda.nield@alu.ing.unlp.edu.ar>
2025-09-02 17:45:06 -0300

    cors

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  
+  // Manejar preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
