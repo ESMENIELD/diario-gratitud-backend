999332ff145c172ef9ef3be926c0faf117e19585
ESMENIELD <esmeralda.nield@alu.ing.unlp.edu.ar>
2025-09-03 13:49:43 -0300

    corsAUTH

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+res.header(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+);
+
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   
   // Manejar preflight requests
